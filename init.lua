cd768e06ca3a58a141e7aab85df47f5dd39ff678
hassan <hassannawaz1210@gmail.com>
2026-03-06 01:54:24 +0500

    update plugins' deprecated statements

--- init.lua
+++ init.lua
@@ -412,7 +412,7 @@ require('lazy').setup({
     end,
   },
   {
-    'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     config = function()
       local leap = require 'leap'
       leap.opts.case_sensitive = true
@@ -1045,7 +1045,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
